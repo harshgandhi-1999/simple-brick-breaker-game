ae836805897eb4e26ec952a8b62b07cf2746632a
harsh gandhi <harshgandhi043@gmail.com>
2020-11-15 19:25:33 +0530

    paddle movement on touch devices

--- scripts.js
+++ scripts.js
@@ -277,7 +277,7 @@ document.addEventListener("click", function () {
 });
 document.addEventListener("keydown", keyDown);
 document.addEventListener("keyup", keyUp);
-document.addEventListener("mousemove", function (e) {
+document.addEventListener("touchmove mousemove", function (e) {
   if (gameStarted === true) {
     var relativeX = e.clientX - canvas.offsetLeft;
     if (
